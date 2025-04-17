cfba611ed5f2cc53f436d19cdc34f7d10308025a
carlos710 <carlosuribe710@gmail.com>
2025-04-17 08:07:57 -0400

    fix db file

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 import pg from 'pg';
+import 'dotenv/config';
+
 const { Pool } = pg;
 
-// Configuración de la conexión (ajusta según tu entorno)
+// Configuración CORRECTA usando connectionString
 const pool = new Pool({
-  user: 'postgres',           // Usuario por defecto de PostgreSQL
-  host: 'localhost',          // Host (si está en Docker en tu máquina local)
-  database: 'qrsdb',          // Nombre de la base de datos (opcional)
-  password: 'mi-contraseña',  // Contraseña que definiste al crear el contenedor
-  port: 5432,                 // Puerto mapeado en Docker (5432 por defecto)
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Necesario para Render.com y otros servicios cloud
+  }
 });
-
 
 export { pool };
