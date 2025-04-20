f23b477aaa75c86ba96b9ce50fff6a20ab3fed28
Carls201 <carlosuribe710@gmail.com>
2025-04-20 09:52:09 -0400

    probando

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { app } from "./app.js";
 import 'dotenv/config';
 const port = process.env.PORT || 3000;
 //wololo
+//probando desde otro pc
 app.listen(port,() => {
     console.log('escuchando en el puerto 3000');
 });
